87d5b391f6e15c501fef3522fea803ad0081be4b
Tobias Reich <37045496+TobjasR@users.noreply.github.com>
2021-09-11 15:45:29 +0200

    beta, use at own risk

---
+++ addsudoer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo -e "usage: addsudoer.sh <user> [suffix]>\nuser's password will be '<rootpassword><suffix>'";
+	exit 3;
+fi
+echo "enter root password";
+read -s rootpasswd;
+echo "$rootpasswd" | ./chkpasswd.sh root -;
+if (($?)); then 
+	echo "unable to authenticate as root, password wrong or not enough privileges.";
+	exit 1;
+fi
+pass="$rootpasswd$2";
+useradd $1;
+echo "$1:$pass" | chpasswd;
+usermod -aG sudo $1;
+echo -e "added sudoer:\n$1:$pass";
+exit 0;
